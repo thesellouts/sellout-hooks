3c73919cc15c4d10177c98de451de96cebc1c9e4
taayyohh <milesxbridges@gmail.com>
2024-06-24 13:20:35 -0400

    update package json

--- src/config/WagmiConfig.ts
+++ src/config/WagmiConfig.ts
@@ -1,11 +1,23 @@
-import { Config } from '@wagmi/core'
-import { sepolia, zora } from 'viem/chains'
+import {
+  arbitrumNova,
+  base,
+  mainnet,
+  optimism,
+  sepolia,
+  zora,
+  zoraSepolia
+} from 'viem/chains'
 import { createConfig, http } from 'wagmi'
 
-export const wagmiConfig: Config = createConfig({
-  chains: [sepolia, zora],
+export const wagmiConfig = createConfig({
+  chains: [mainnet, sepolia, zora, zoraSepolia, optimism, base, arbitrumNova],
   transports: {
+    [mainnet.id]: http(),
     [sepolia.id]: http(),
-    [zora.id]: http()
+    [arbitrumNova.id]: http(),
+    [zora.id]: http(),
+    [zoraSepolia.id]: http(),
+    [optimism.id]: http(),
+    [base.id]: http()
   }
-}) as Config
+})
